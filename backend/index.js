57d5fe865d1c08fb967201d7fb0e38aad3506534
Payal Sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-10-24 15:23:15 +0530

    DB connection

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { connection } = require("./db");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log(`port is running at ${process.env.port}`);
+    console.log("connected to DB");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
