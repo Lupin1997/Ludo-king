e71d1b50b20faec258cf4696975ad2d43a700a9d
Payal Sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-10-25 10:57:05 +0530

    Added routes

---
+++ frontend/src/Routes/AllRoute.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+
+
+
+
+function Allroute(){
+    return <div>
+        <Routes>
+            <Route path="/" element={}/>
+            <Route path="/welcome" element={}/>
+            <Route path="/register" element={}/>
+            <Route path="/home" element={}/>
+            <Route path="/ludo/:id" element={}/>
+            
+        </Routes>
+
+    </div>
+}
+export default Allroute
